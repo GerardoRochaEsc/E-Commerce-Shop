6561f91e6b990f4bf33805897b98bb543c6a88b9
Gerardo Rocha <101081681+GerardoRochaEsc@users.noreply.github.com>
2023-11-02 23:34:25 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,66 @@
+//Products
+const products = {
+    //Coats
+    {
+        id: "coat-01",
+        tittle: "Cout 01",
+        image:"./img/coats/01.jgp",
+        category:{
+            name: "Coats",
+            id: "coats"
+        },
+        price: 1000
+    },
+    {
+        id: "coat-02",
+        tittle: "Cout 02",
+        image:"./img/coats/02.jgp",
+        category:{
+            name: "Coats",
+            id: "coats"
+        },
+        price: 1000
+    }
+    {
+        id: "coat-03",
+        tittle: "Cout 03",
+        image:"./img/coats/03.jgp",
+        category:{
+            name: "Coats",
+            id: "coats"
+        },
+        price: 1000
+    }
+    {
+        id: "coat-04",
+        tittle: "Cout 04",
+        image:"./img/coats/04.jgp",
+        category:{
+            name: "Coats",
+            id: "coats"
+        },
+        price: 1000
+    }
+    {
+        id: "coat-05",
+        tittle: "Cout 05",
+        image:"./img/coats/05.jgp",
+        category:{
+            name: "Coats",
+            id: "coats"
+        },
+        price: 1000
+    }
+    //T-shirts
+    {
+        id: "tshirt-01",
+        tittle: "Tshirt 01",
+        image: "./img/tshirt/01.jpg",
+        category:{
+            name: "Tshirts",
+            id: "tshirts",
+        },
+        price:1000
+    }
+
+}
